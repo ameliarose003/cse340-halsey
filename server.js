fa63938c38108e0fc278ae33bdb15f92ad785942
Ami Halsey <cro19030@byui.edu>
2025-09-17 14:55:21 -0600

    Set up ejs routes, partials, etc and attatched restore.js

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url'; // Why do I need this???
+
+// Derive __dirname for ES Modules: Why do I need this??
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-const name = process.env.NAME;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
 
+// Routes
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
+    const title = 'Welcome Home';
+    res.render('home', {title});
 });
 
-app.get('/newRoute', (req, res) => {
-    res.send('This is a new route!');
+app.get('/about', (req, res) => {
+    const title = 'About Me';
+    res.render('about', {title});
 });
 
-const PORT = 3000;
+app.get('/products', (req,res) => {
+    const title = 'Our Products';
+    res.render('products', {title});
+})
+
+
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://127.0.0.1:${PORT}`)
