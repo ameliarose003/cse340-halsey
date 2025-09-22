598d48704fbd46bdbcbdab8caaefcd2fc3392e88
Ami Halsey <cro19030@byui.edu>
2025-09-22 14:42:47 -0600

    added about me info

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url'; // Why do I need this???
+import { fileURLToPath } from 'url'; // Why do I need this??? It is needed because __fileName and __dirname aren't built-in
 
-// Derive __dirname for ES Modules: Why do I need this??
+// Derive __dirname for ES Modules: Why do I need this?? Same reason as above
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const PORT = process.env.PORT || 3000;
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV.toLowerCase() || 'production';
+
+    next();
+})
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
@@ -27,9 +38,24 @@ app.get('/products', (req,res) => {
     res.render('products', {title});
 })
 
+if (NODE_ENV.includes('dev')) {
+    const ws = await import('ws');
 
-const NODE_ENV = process.env.NODE_ENV || 'production';
-const PORT = process.env.PORT || 3000;
+    try {
+        const wsPort = parseInt(PORT) + 1;
+        const wsServer = new ws.WebSocketServer({ port: wsPort});
+        
+        wsServer.on('listening', () => {
+            console.log(`WebSocket server running on port ${wsPort}`);
+        });
+
+        wsServer.on('error', (error) => {
+            console.error('WebSocket server error:', error);
+        });
+    } catch (error) {
+        console.error('Failed to start WebSocket server:', error);
+    }
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on http://127.0.0.1:${PORT}`)
